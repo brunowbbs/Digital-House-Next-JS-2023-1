9b4557c7a215778e195567e63b58aa39c4e49b29
Wesley Bruno <engwesleybruno@gmail.com>
2023-03-06 20:12:58 -0300

    a

---
+++ aula-07-api-routes-nextJS/src/data/index.ts
@@ -0,0 +1,16 @@
+const TASKS = [
+  {
+    id: 1,
+    name: "Task de teste",
+  },
+  {
+    id: 2,
+    name: "Task de teste 2",
+  },
+  {
+    id: 3,
+    name: "Task de teste 3",
+  },
+];
+
+export default TASKS;
